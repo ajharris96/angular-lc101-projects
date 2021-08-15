27e5f50f68f7f1a03ec9176cebd17a17384993e3
Andrew <andrew300@priv.lan>
2021-08-15 18:29:02 -0400

    done with part1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'My Favorite Photos';
+  photosTitle = 'Some Reddit Photos';
   image1 = 'https://preview.redd.it/axfdkc3bojh71.jpg?width=640&crop=smart&auto=webp&s=c25c81afac6bc202eb3981000c3a3e73a6042da0';
   image2 = 'https://preview.redd.it/zqcckk5gls961.png?width=640&crop=smart&auto=webp&s=aade0d53ba02f92bc3da0b0aa98b89c4236f5d39'
   image3 = 'https://preview.redd.it/615o3mx0uic61.jpg?width=640&crop=smart&auto=webp&s=004fe6902564d7d8b61c4512aa97622cdf8f180d'
