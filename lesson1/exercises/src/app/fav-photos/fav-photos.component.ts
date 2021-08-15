e78755be91715ba1641136c08f25eb6484876e17
Andrew <andrew300@priv.lan>
2021-08-15 18:21:28 -0400

    part1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,11 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
-
+  photosTitle = 'My Favorite Photos';
+  image1 = 'https://preview.redd.it/axfdkc3bojh71.jpg?width=640&crop=smart&auto=webp&s=c25c81afac6bc202eb3981000c3a3e73a6042da0';
+  image2 = 'http://www.nasa.gov/sites/default/files/thumbnails/image/pia23645_pbd_main-16.jpg';
+  image3 = 'https://media.sanctuarymentalhealth.org/wp-content/uploads/2021/03/04151535/The-Starry-Night.jpg'
   constructor() { }
 
   ngOnInit() {
